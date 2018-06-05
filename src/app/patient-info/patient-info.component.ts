ae2d37d7392a1481044617bcdece821aa1751cbd
Judis <Jason.Judis@cchmc.org>
2018-06-05 12:01:36 -0400

    made heavy changes to the patient info tab

--- src/app/patient-info/patient-info.component.ts
+++ src/app/patient-info/patient-info.component.ts
@@ -14,15 +14,20 @@ export class PatientInfoComponent implements OnInit {
   patient: Patient;
   observationList: Observation[] = [];
   weightList: any[] = [];
-
+  temperatureList: any[] = [];
+  hemoglobinList: any[] = [];
+  bilirubinList: any[] = [];
+  smokingString: any[] = [];
   constructor(public epicAuthService: EpicAuthService, public fhirService: FhirService) {
   }
 
   ngOnInit() {
+    // gets patient
     this.epicAuthService.completeLoginWithCode().then(_ => {
         this.getPatient();
       }
     );
+    // code for weights
     this.fhirService.getObservations('29463-7').subscribe(
       obs => {
         this.observationList.push(obs);
@@ -30,6 +35,38 @@ export class PatientInfoComponent implements OnInit {
         this.weightList.push(obs.valueQuantity);
       }
     );
+    // code for temperature
+    this.fhirService.getObservations('8310-5').subscribe(
+      obs => {
+        this.observationList.push(obs);
+
+        this.temperatureList.push(obs.valueQuantity);
+      }
+    );
+    // code for hemoglobin reading
+    this.fhirService.getObservations('718-7').subscribe(
+      obs => {
+        this.observationList.push(obs);
+
+        this.hemoglobinList.push(obs.valueQuantity);
+      }
+    );
+    // code for bilirubin
+    this.fhirService.getObservations('1975-2').subscribe(
+      obs => {
+        this.observationList.push(obs);
+
+        this.bilirubinList.push(obs.valueQuantity);
+      }
+    );
+    /* code for current smoking history (cant get it to work)
+     this.fhirService.getObservations('72166-2').subscribe(
+      obs => {
+        this.observationList.push(obs);
+
+        this.smokingString.push(obs._valueBoolean);
+      }
+    ); */
   }
   getPatient() {
     this.fhirService.getPatient().subscribe( pt =>
