d245e7972dfc43b26a69b196ae74307e3846a2db
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2018-06-07 12:03:49 -0400

    working on oauth

--- src/app/smart-auth/epic-auth.service.ts
+++ src/app/smart-auth/epic-auth.service.ts
@@ -1,29 +1,25 @@
 import {Injectable} from '@angular/core';
-import {epicSmartAuthConfig} from './smart-config';
+import {baseSmartAuthConfig, epicSmartAuthConfig} from './smart-config';
 import {OAuthService} from '../../angular-oauth2-oidc/oauth-service';
 import {NullValidationHandler} from '../../angular-oauth2-oidc/token-validation/null-validation-handler';
 import {FhirApiEndpoint} from './fhir-api-endpoint';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthConfig} from '../../angular-oauth2-oidc/auth.config';
-import Resource = fhir.Resource;
+import CapabilityStatement = fhir.CapabilityStatement;
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpicAuthService {
-  serviceUri: '';
-
   constructor(public oauthService: OAuthService, private http: HttpClient) {
-    this.configureWithNewConfigApi();
+    // this.configureWithNewConfigApi(epicSmartAuthConfig);
+
+    console.log('issuer: ' + this.oauthService.issuer);
   }
 
   login(): void {
     this.oauthService.initAuthorizationCodeFlow();
-  }
-
-  getServiceUri(): string {
-    return this.serviceUri;
   }
 
   getEndpoints(): Observable<FhirApiEndpoint[]> {
@@ -33,15 +29,40 @@ export class EpicAuthService {
   }
 
   generateAuthConfig(endpoint: string): Observable<AuthConfig> {
+    // const origEndpoint = 'https://boomertest.cchmc.org/fhir/api/FHIR/DSTU2/';
+    const origEndpoint = 'https://open-ic.epic.com/argonaut/api/FHIR/Argonaut/';
+
     endpoint = '/assets/open-epic-metadata.json';
 
     return new Observable<AuthConfig>(subscriber => {
       // get auth config from URI
-      this.http.get<Resource>(endpoint).subscribe((data: Resource) => {
-        console.log('received this conformance statement');
-        console.log(data);
-      });
+      const config: AuthConfig = Object.assign({}, baseSmartAuthConfig);
 
+      this.http.get<CapabilityStatement>(endpoint).subscribe((data: CapabilityStatement) => {
+        config.issuer = origEndpoint; // TODO: When pulling from endpoint, remove this
+
+        try {
+
+          for (const ext of data.rest[0].security.extension[0].extension) {
+            // console.log(ext);
+
+            switch (ext.url) {
+              case 'authorize':
+                config.loginUrl = ext.valueUri;
+                // console.log('loginUrl: ' + ext.valueUri);
+                break;
+              case 'token':
+                config.tokenEndpoint = ext.valueUri;
+                // console.log('tokenUrl: ' + ext.valueUri);
+                break;
+            }
+          }
+
+          this.configureWithNewConfigApi(config);
+        } catch (e) {
+          console.log(e);
+        }
+      });
     });
   }
 
@@ -57,8 +78,11 @@ export class EpicAuthService {
     });
   }
 
-  private configureWithNewConfigApi() {
-    this.oauthService.configure(epicSmartAuthConfig);
+  private configureWithNewConfigApi(config: AuthConfig) {
+    console.log('CONFIGURATION: ');
+    console.log(config);
+
+    this.oauthService.configure(config);
     this.oauthService.tokenValidationHandler = new NullValidationHandler();
     this.oauthService.setStorage(sessionStorage);
   }
