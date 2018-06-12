f3d3dd697fbaa6fd971369821666e77827a67e9b
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2018-06-12 14:37:50 -0400

    add firebase information to environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyChiQ4vl0W6FVghoElBD4cHghEzstnvDUI',
+    authDomain: 'smart-fhir-sandbox.firebaseapp.com',
+    databaseURL: 'https://smart-fhir-sandbox.firebaseio.com',
+    projectId: 'smart-fhir-sandbox',
+    storageBucket: 'smart-fhir-sandbox.appspot.com',
+    messagingSenderId: '271134181953'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'your-api-key',
-    authDomain: 'your-auth-domain',
-    databaseURL: 'your-db-url',
-    projectId: 'your-project-id',
-    storageBucket: 'your-storage-bucket',
-    messagingSenderId: 'your-messaging-sender-id'
+    apiKey: 'AIzaSyChiQ4vl0W6FVghoElBD4cHghEzstnvDUI',
+    authDomain: 'smart-fhir-sandbox.firebaseapp.com',
+    databaseURL: 'https://smart-fhir-sandbox.firebaseio.com',
+    projectId: 'smart-fhir-sandbox',
+    storageBucket: 'smart-fhir-sandbox.appspot.com',
+    messagingSenderId: '271134181953'
   }
 };
 
