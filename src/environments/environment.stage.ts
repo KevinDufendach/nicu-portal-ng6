a6e8c7df3a66e93282957aad5b6bd5ccdba944a9
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2018-06-13 15:33:09 -0400

    add stage configuration environment

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  'output-path': './dist/nicu-portal-stage/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwoIYzawEDmVwEbke8h6b08zqrvCYRhWM',
+    authDomain: 'nicu-portal-staging.firebaseapp.com',
+    databaseURL: 'https://nicu-portal-staging.firebaseio.com',
+    projectId: 'nicu-portal-staging',
+    storageBucket: 'nicu-portal-staging.appspot.com',
+    messagingSenderId: '191848905130'
+  }
+};
